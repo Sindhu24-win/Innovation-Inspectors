d05751dcfa6f6a743846c785c0ab74ab80fb12e6
Meenaa Kannan <meenaa.kannan1@outlook.com>
2025-03-07 09:05:02 -0800

    Add files via upload
    src/test/java
    src/test/reources

---
+++ src/test/java/StepDefinitions/TreeSteps.java
@@ -0,0 +1,195 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TreeSteps {
+
+@Given("The user is in the Home page after Sign in")
+public void the_user_is_in_the_home_page_after_sign_in() {
+    
+}
+
+@When("The user clicks the Getting Started button in Tree Panel")
+public void the_user_clicks_the_getting_started_button_in_tree_panel() {
+   
+}
+
+@When("The user select Tree item from the drop down menu")
+public void the_user_select_tree_item_from_the_drop_down_menu() {
+    
+}
+
+@Then("The user be directed to {string} Data Structure Page")
+public void the_user_be_directed_to_data_structure_page(String string) {
+    
+}
+
+@Given("The user is on the Home page after signing in")
+public void the_user_is_on_the_home_page_after_signing_in() {
+    
+}
+
+@When("The user selects Tree item from the drop-down menu")
+public void the_user_selects_tree_item_from_the_drop_down_menu() {
+   
+}
+
+@Then("The user should land on the {string} Data Structure page")
+public void the_user_should_land_on_the_data_structure_page(String string) {
+    
+}
+
+@Given("The user is on the {string} Data Structure page after signing in")
+public void the_user_is_on_the_data_structure_page_after_signing_in(String string) {
+    
+}
+
+@When("The user clicks {string} link")
+public void the_user_clicks_link(String string) {
+   
+}
+
+@Then("The user should be redirected to {string} page")
+public void the_user_should_be_redirected_to_page(String string) {
+    
+}
+
+@Given("The user is on the {string} page")
+public void the_user_is_on_the_page(String string) {
+    
+}
+
+@When("The user clicks {string} button in the Overview of Trees page")
+public void the_user_clicks_button_in_the_overview_of_trees_page(String string) {
+    
+}
+
+@Then("The user should be redirected to a page having a Try Editor with a Run button to test")
+public void the_user_should_be_redirected_to_a_page_having_a_try_editor_with_a_run_button_to_test() {
+   
+}
+
+@Given("The user is on the Try Editor page")
+public void the_user_is_on_the_try_editor_page() {
+   
+}
+
+@When("The user clicks the Run button without entering code in the Editor")
+public void the_user_clicks_the_run_button_without_entering_code_in_the_editor() {
+   
+}
+
+@Then("The user should see an error message in an alert window")
+public void the_user_should_see_an_error_message_in_an_alert_window() {
+    
+}
+
+@When("The user writes invalid code in the Editor and clicks the Run button")
+public void the_user_writes_invalid_code_in_the_editor_and_clicks_the_run_button() {
+    
+}
+
+@When("The user writes valid code in the Editor and clicks the Run button")
+public void the_user_writes_valid_code_in_the_editor_and_clicks_the_run_button() {
+   
+}
+
+@Then("The user should see output in the console")
+public void the_user_should_see_output_in_the_console() {
+    
+}
+
+@Given("The user is on the Tree page after signing in")
+public void the_user_is_on_the_tree_page_after_signing_in() {
+    
+}
+
+@When("The user clicks {string} button in the Terminologies page")
+public void the_user_clicks_button_in_the_terminologies_page(String string) {
+   
+}
+
+@When("The user clicks {string} button in the Types of Trees page")
+public void the_user_clicks_button_in_the_types_of_trees_page(String string) {
+    
+}
+
+@When("The user clicks {string} button in the Tree Traversal page")
+public void the_user_clicks_button_in_the_tree_traversal_page(String string) {
+    
+}
+
+@When("The user clicks {string} button in the Traversals-Illustration page")
+public void the_user_clicks_button_in_the_traversals_illustration_page(String string) {
+   
+}
+
+@When("The user clicks {string} button in the Binary trees page")
+public void the_user_clicks_button_in_the_binary_trees_page(String string) {
+    
+}
+
+@When("The user clicks {string} button in the Types of Binary Trees page")
+public void the_user_clicks_button_in_the_types_of_binary_trees_page(String string) {
+    
+}
+
+@When("The user clicks {string} button in the Implementation in Python page")
+public void the_user_clicks_button_in_the_implementation_in_python_page(String string) {
+   
+}
+
+@When("The user clicks the {string} button")
+public void the_user_clicks_the_button(String string) {
+   
+}
+
+@Then("The user should be redirected to a page having a try Editor with a Run button to test")
+public void the_user_should_be_redirected_to_a_page_having_a_try_Editor_with_a_run_button_to_test() {
+    
+}
+
+@Given("The user is in the tryEditor page for {string}")
+public void the_user_is_in_the_try_editor_page_for(String string) {
+   
+}
+
+@When("The user clicks the {string} without entering code")
+public void the_user_clicks_the_without_entering_code(String string) {
+    
+}
+
+@Then("The user should see an error message in the alert window")
+public void the_user_should_see_an_error_message_in_the_alert_window() {
+    
+}
+
+@When("The user writes invalid python code and clicks the {string}")
+public void the_user_writes_invalid_python_code_and_clicks_the(String string) {
+    
+}
+
+@When("The user writes valid python code and clicks the {string}")
+public void the_user_writes_valid_python_code_and_clicks_the(String string) {
+  
+}
+
+@Given("The user is in the {string} data structure page after sign in")
+public void the_user_is_in_the_data_structure_page_after_sign_in(String string) {
+   
+}
+
+@When("The user clicks the {string} link")
+public void the_user_clicks_the_link(String string) {
+    
+}
+
+@Then("The user should be redirected to the {string} page")
+public void the_user_should_be_redirected_to_the_page(String string) {
+   
+}
+
+
+}
